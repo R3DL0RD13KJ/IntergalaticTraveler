5c89db8141ae700ff9e96970bc6a97dff8d4ea54
Kenneth Johnson <kd123johnson@gmail.com>
2021-10-19 09:30:41 -0600

    Initial Commit

---
+++ IntergalaticTraveler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntergalaticTraveler
+//
+//  Created by Kenneth Johnson on 10/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
