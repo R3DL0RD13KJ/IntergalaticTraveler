40238ed92c378126bfeef83a60b2a33c172e1146
Kenneth Johnson <kd123johnson@gmail.com>
2021-10-20 10:44:55 -0600

    Segue Complete

--- IntergalaticTraveler/ViewController.swift
+++ IntergalaticTraveler/ViewController.swift
@@ -11,9 +11,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        assignbackground()
         // Do any additional setup after loading the view.
     }
+    func assignbackground(){
+            let background = UIImage(named: "Space-Bg")
 
+            var imageView : UIImageView!
+            imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+            imageView.clipsToBounds = true
+            imageView.image = background
+            imageView.center = view.center
+            view.addSubview(imageView)
+            self.view.sendSubviewToBack(imageView)
+        }
 
 }
 
